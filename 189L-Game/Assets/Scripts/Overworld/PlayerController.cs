76f9710e2e9bd7afe8a2f59303e01749a0ddda32
Jordan Wong <jordanhwong1@gmail.com>
2023-05-25 17:09:18 -0700

    Hard Coding Single Press

--- 189L-Game/Assets/Scripts/Overworld/PlayerController.cs
+++ 189L-Game/Assets/Scripts/Overworld/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
    [SerializeField] private LayerMask collisionLayer;
    private Transform movePoint;
    private bool canMove = true;
+   private const float tileDistance = 1.0f;
 
     private void Awake()
     {
@@ -35,14 +36,24 @@ public class PlayerController : MonoBehaviour
 
     private void CheckForInput()
     {
-        if ((Mathf.Abs(Input.GetAxisRaw("Horizontal")) >= 0.5f) && NotCollisionTile(Input.GetAxisRaw("Horizontal"), 0.0f))
+        if (Input.GetKeyDown(KeyCode.A) && NotCollisionTile(-tileDistance, 0.0f))
         {
-            movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, 0.0f);
+            this.movePoint.position += new Vector3(-tileDistance, 0.0f, 0.0f);
             canMove = false;
         }
-        else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) >= 0.5f && NotCollisionTile(0.0f, Input.GetAxisRaw("Vertical")))
+        else if (Input.GetKeyDown(KeyCode.D) && NotCollisionTile(tileDistance, 0.0f))
         {
-            movePoint.position += new Vector3(0.0f, Input.GetAxisRaw("Vertical"), 0.0f);
+            this.movePoint.position += new Vector3(tileDistance, 0.0f, 0.0f);
+            canMove = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && NotCollisionTile(0.0f, tileDistance))
+        {
+            this.movePoint.position += new Vector3(0.0f, tileDistance, 0.0f);
+            canMove = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && NotCollisionTile(0.0f, -tileDistance))
+        {
+            this.movePoint.position += new Vector3(0.0f, -tileDistance, 0.0f);
             canMove = false;
         }
     }
@@ -56,7 +67,7 @@ public class PlayerController : MonoBehaviour
     }
     private bool NotCollisionTile(float displacementX, float displacementY)
     {
-        if (Physics2D.OverlapCircle(this.movePoint.position + new Vector3(displacementX, displacementY, 0.0f), 0.5f, collisionLayer))
+        if (Physics2D.OverlapBox(this.movePoint.position + new Vector3(displacementX, displacementY, 0.0f), new Vector2(tileDistance - 0.1f, tileDistance - 0.1f), 0.0f, collisionLayer))
         {
             return false;
         }
